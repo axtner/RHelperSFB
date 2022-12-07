de58dddd8a007245ea77a77f61f4e43be27208d9
axtner <42058176+axtner@users.noreply.github.com>
2022-12-07 16:55:40 +0100

    function rep_tab created change of query function in res_tab

--- R/res_tab.R
+++ R/res_tab.R
@@ -4,15 +4,15 @@
 #' 
 #' Lists the assignments for the different taxonomic levels and calculated the mean log probability for those assignments made by PROTAX. Also lists the reference with the highest similarity and the average similarity score. To connect to the PostgreSQL database from outside the IZW domain it would need ssh connection to the database host and port forwarding to 5433. The db_host must be set to "localhost" then. 
 #' 
-#' @param sample_id String defining a sample ID (optional). Strings can be complete or incomplete sample IDs (e.g. "VDBC" or c("VDBC068_1_1", "VDBC=62_1_2")).
-#' @param rep_id String defining a replicate ID (optional). Strings can be complete or incomplete replicate IDs (e.g. "rep1" or c("rep1", "rep2")). 
+#' @param sample_id String defining a sample ID (optional). Strings can be complete or incomplete sample IDs (e.g. "VDBC" or c("VBDC068_1_1", "VBDC_062_1_2")). Either sample_id or rep_id (or both) must be provided.
+#' @param rep_id String defining a replicate ID (optional). Strings can be complete or incomplete replicate IDs (e.g. "rep1" or c("rep1", "rep2")). Either sample_id or rep_id (or both) must be provided.
 #' @param dir Custom output directory (optional), default is the current working directory (getwd()).
-#' @param file_out Custom name of output file (optional). Output file is a tab delimited text file ("*.txt"). Default creates a file name starting with "AssignResOut", followed by sample_id and rep_id (if provided) and date (e.g. "AssignResOut_20221207_105429.txt" or "AssignResOut_VDBC_068_1_1_rep1_20221207_105429.txt"). 
+#' @param file_out Custom name of output file (optional). Output file is a tab delimited text file ("*.txt"). Default creates a file name starting with "AssignResOut", followed by sample_id and rep_id (if provided) and date (e.g. "AssignResOut_20221207_105429.txt" or "AssignResOut_VBDC_068_1_1_rep1_20221207_105429.txt"). 
 #' @param db_name String giving the database name (optional). Default is the biodiv database.
 #' @param db_user String giving the name of the database user (optional). Default user has only SELECT privileges.
 #' @param db_pwd String giving the password of the database user (optional). Default user has only SELECT privileges.
 #' @param db_host String giving the internal IP address of the host server of the biodiv database. If you wish to connect to another database you have to adjust db_name and db_host accordingly. To connect to the PostgreSQL database from outside the IZW domain it would need ssh connection to the database host and port forwarding to 5433. The db_host must be set to "localhost" then.
-#' @param db_port String giving the port the Postgres server listens to. Default is set to "5433", the port bidov database listens to.
+#' @param db_port String giving the port the Postgres server listens to. Default is set to "5433", the port biodiv database listens to.
 #' @examples
 #' Simple query with default settings would give all results present in the database (not recommended): 
 #' res_tab()
@@ -82,7 +82,7 @@ res_tab = function(sample_id = NA,
   # establish db connection
   db = DBI::dbConnect(RPostgres::Postgres(), host = db_host, user = db_user, password = db_pwd, dbname = db_name, port = db_port)
   
-  q_tab = DBI::dbGetQuery(db, paste0(
+  query = dbSendQuery(db, paste0(
     "
     SELECT
       tab1.sample_id, 
@@ -166,13 +166,17 @@ res_tab = function(sample_id = NA,
     ")
     )
   
+  q_tab = DBI::dbFetch(query)
+  
+  DBI::dbClearResult(query)
+  
   if(nrow(q_tab) == 0){
     message(paste0("No results for found. Please check for correct sample_id or rep_id if provided"))
   } else {
     # save q_tab as comma tab-separated text file
     write.table(q_tab, file = file_out, sep = "\t")
     
-    message(paste0("\nYou queried ", nrow(q_tab), " sequences from the \"", db_name, "\" database. \nResults are written to the ", paste0("\"", dir, "/", file_out, "\""), " file.", "\n"))
+    message(paste0("\nYou queried ", nrow(q_tab), " assignments from the \"", db_name, "\" database. \nResults are written to the ", paste0("\"", dir, "/", file_out, "\""), " file.", "\n"))
   } 
   # disconnect from database
   DBI::dbDisconnect(db)
